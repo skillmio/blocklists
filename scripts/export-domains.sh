1fbeca3060c55b4d1c79f733514e1310864124ca
Skillmio <skillmiocfs@gmail.com>
2026-02-14 13:07:32 +0100

    Update export-domains.sh

--- scripts/export-domains.sh
+++ scripts/export-domains.sh
@@ -43,5 +43,6 @@ echo "  exempt.txt (downloaded)"
 echo "  all_blocked_domains.txt.bak (backup)"
 
 echo ""
-echo "Next Step: scp $(whoami)@$(hostname -I | awk '{print $1}'):/tmp/clean_blocked_domains.txt . "
+echo 'Next Step: scp $(whoami)@$(hostname -I | awk "{print $1}"):/tmp/clean_blocked_domains.txt "%USERPROFILE%\Downloads" '
+
 
